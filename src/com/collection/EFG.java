238120662be49bc62ffa69e8455f5f25b8cd3f5a
zhang <2684378787@qq.com>
2021-11-20 19:37:03 +0800

    测试代码:写算法用途

---
+++ src/com/collection/EFG.java
@@ -0,0 +1,11 @@
+package com.collection;
+
+public class EFG {
+
+
+    public static void main(String[] args) {
+        System.out.println("Aaaaaaaaaaa");
+        System.out.println("zzzzzzzzzz");
+
+    }
+}
